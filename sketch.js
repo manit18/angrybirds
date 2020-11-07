365217ba08411ddac7e304997b708f4310e272a5
Manit Dvivedi <manitdivedi@gmail.com>
2020-11-07 16:59:55 +0530

    newpig

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig2;
 var backgroundImg,platform;
 
 function preload() {
@@ -33,7 +33,8 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    
+    pig2 = new Pig(150,150);
     bird = new Bird(100,100);
 
 }
@@ -53,6 +54,7 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
+    pig2.display();
     log3.display();
 
     box5.display();
